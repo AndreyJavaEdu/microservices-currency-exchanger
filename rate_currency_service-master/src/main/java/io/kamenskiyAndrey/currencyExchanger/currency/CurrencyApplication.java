b0c1e287203e65dfa32ab1ffe51784912f351bab
AndreyJavaEd <scream123456@yandex.ru>
2023-12-07 23:08:04 +0300

    В application.yml сделали настройку регистрации клиента в сервесе Еврика как false. создали дополнительный файл конфигурации application-cloud.yml для профиля когда сервер Еврики запучщен. Аналогично сконфигурировали сервис currency-rate-service. Аннотировали класс запуска сервиса currency-rate-service дополнительной аннотацией @EnableDiscoveryClient. Также перевели версию спринга с 3.1.6 на 3.2.0 для совместной работы со Spring-cloud, прежняя версия была несовместима. Также добавили в pom зависимость для регистрации клиента в Еврике: spring-cloud-starter-netflix-eureka-client.
    Прописали в dependencyManagement spring-cloud-dependencies, также внедрили репозиторий Spring Milestones - т.к. Spring cloud не добавлялся

--- rate_currency_service-master/src/main/java/io/kamenskiyAndrey/currencyExchanger/currency/CurrencyApplication.java
+++ rate_currency_service-master/src/main/java/io/kamenskiyAndrey/currencyExchanger/currency/CurrencyApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("io.kamenskiyAndrey.currencyExchanger.currency.config")
+@EnableDiscoveryClient
 public class CurrencyApplication {
 
 	public static void main(String[] args) {
