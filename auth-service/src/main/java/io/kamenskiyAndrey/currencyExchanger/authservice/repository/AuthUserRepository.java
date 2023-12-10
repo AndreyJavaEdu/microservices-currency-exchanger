296fa6b3e7c69e7a848dccf4a9973f2280ab31d0
AndreyJavaEd <scream123456@yandex.ru>
2023-12-10 04:08:34 +0300

    В интерфейсе AuthUserRepository обозначили нестандартный метод как поиск пользователя по его имени - AuthUserRepository(). Этот метод будет иметь тип Optional.

--- auth-service/src/main/java/io/kamenskiyAndrey/currencyExchanger/authservice/repository/AuthUserRepository.java
+++ auth-service/src/main/java/io/kamenskiyAndrey/currencyExchanger/authservice/repository/AuthUserRepository.java
@@ -4,7 +4,10 @@ import io.kamenskiyAndrey.currencyExchanger.authservice.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AuthUserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity>findByUserName(String name);
 
 }
