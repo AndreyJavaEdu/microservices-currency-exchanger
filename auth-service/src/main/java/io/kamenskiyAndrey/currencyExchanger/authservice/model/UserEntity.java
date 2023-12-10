eb370badb32e43d03bb3ad2457c393ed64a31e0b
AndreyJavaEd <scream123456@yandex.ru>
2023-12-10 03:06:17 +0300

    Аннотировали класс UserEntity аннотациями из проекта Lombok для конструкторов и для обозначения класса как Entity. Обозначили таблицу аннотацией @Table. Ввели поля id пользователя, его имя и его пароль.

---
+++ auth-service/src/main/java/io/kamenskiyAndrey/currencyExchanger/authservice/model/UserEntity.java
@@ -0,0 +1,67 @@
+package io.kamenskiyAndrey.currencyExchanger.authservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "AUTH_USER")
+public class UserEntity implements UserDetails {
+    @Id
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "user_name")
+    private Integer userName;
+
+
+    @Column(name = "user_password")
+    private Integer userPassword;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
