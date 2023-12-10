4cfec273a1d6cc4b4221ade503f57cb275b4972d
AndreyJavaEd <scream123456@yandex.ru>
2023-12-10 06:49:02 +0300

    Будем загружать пользователей в базу H2 при каждом запуске приложения - для этого создали специальный класс AuthUsersLoader. Чтобы он запускался перед запуском приложения и выполнял какие то действия имлементируем интерфейс CommandLineRunner. Переопределили мето run() интерфейса CommandLineRunner. В этом методе загружаем двух пользователей, которые строим билдером.

--- auth-service/src/main/java/io/kamenskiyAndrey/currencyExchanger/authservice/service/AuthUsersLoader.java
+++ auth-service/src/main/java/io/kamenskiyAndrey/currencyExchanger/authservice/service/AuthUsersLoader.java
@@ -1,10 +1,41 @@
 package io.kamenskiyAndrey.currencyExchanger.authservice.service;
 
+import io.kamenskiyAndrey.currencyExchanger.authservice.model.UserEntity;
+import io.kamenskiyAndrey.currencyExchanger.authservice.repository.AuthUserRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+/*
+Класс для загрузки пользователя при старте приложения
+ */
+@Component
+@RequiredArgsConstructor
 public class AuthUsersLoader implements CommandLineRunner {
+
+    private final AuthUserRepository repository;
+
+    //Используем данный Бин для кодирования пароля
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public void run(String... args) throws Exception {
+        //Загружаем первого пользователя
+        repository.save(
+                UserEntity.builder().userId(1)
+                        .userName("pavel").userPassword(passwordEncoder.encode("pavel123"))
+                        .build()
+        );
 
+        //Загружаем второго пользователя
+        repository.save(
+                UserEntity.builder().userId(2)
+                        .userName("artem").userPassword(passwordEncoder.encode("artem123"))
+                        .build()
+        );
     }
 }
