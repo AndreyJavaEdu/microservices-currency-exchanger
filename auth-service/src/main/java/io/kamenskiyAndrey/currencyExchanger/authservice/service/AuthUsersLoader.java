8bfe78142f6635629c81ccafe01dcf4486765310
AndreyJavaEd <scream123456@yandex.ru>
2023-12-10 06:21:25 +0300

    Будем загружать пользователей в базу H2 при каждом запуске приложения - для этого создали специальный класс AuthUsersLoader. Чтобы он запускался перед запуском приложения и выполнял какие то действия имлементируем интерфейс CommandLineRunner.

---
+++ auth-service/src/main/java/io/kamenskiyAndrey/currencyExchanger/authservice/service/AuthUsersLoader.java
@@ -0,0 +1,10 @@
+package io.kamenskiyAndrey.currencyExchanger.authservice.service;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class AuthUsersLoader implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
