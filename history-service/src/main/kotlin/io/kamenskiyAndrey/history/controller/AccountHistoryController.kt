d51737c04625190f7b2fd50f88913bd1605c9d37
AndreyJavaEd <scream123456@yandex.ru>
2023-12-19 19:56:11 +0300

    Начали реализацию РЕСТ контроллера AccountHistoryController для отправки данных по http.

---
+++ history-service/src/main/kotlin/io/kamenskiyAndrey/history/controller/AccountHistoryController.kt
@@ -0,0 +1,11 @@
+package io.kamenskiyAndrey.history.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/history")
+class AccountHistoryController {
+
+
+}
