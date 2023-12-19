bbf5bd67c6d79185856f4cc698f225b23557a6d2
AndreyJavaEd <scream123456@yandex.ru>
2023-12-19 16:51:59 +0300

    Добавли аннотацию на главном классе сервиса с методом мейн - @EnableDiscoveryClient для регистрации сервиса в Eureca сервисе.

--- history-service/src/main/kotlin/io/kamenskiyAndrey/history/HistoryServiceApplication.kt
+++ history-service/src/main/kotlin/io/kamenskiyAndrey/history/HistoryServiceApplication.kt
@@ -2,8 +2,10 @@ package io.kamenskiyAndrey.history
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class HistoryServiceApplication
 
 fun main(args: Array<String>) {
