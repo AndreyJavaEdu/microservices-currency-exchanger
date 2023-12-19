d07d20ca6ddb3e078f7410b7c9e5b1a676752409
AndreyJavaEd <scream123456@yandex.ru>
2023-12-19 17:40:16 +0300

    Заинжектили бин репозитория в классе сервиса AccountEventService

--- history-service/src/main/kotlin/io/kamenskiyAndrey/history/service/AccountEventService.kt
+++ history-service/src/main/kotlin/io/kamenskiyAndrey/history/service/AccountEventService.kt
@@ -1,9 +1,10 @@
 package io.kamenskiyAndrey.history.service
 
+import io.kamenskiyAndrey.history.repository.AccountEventRepository
 import org.springframework.stereotype.Service
 
 @Service
-class AccountEventService {
+class AccountEventService(private val repository: AccountEventRepository) { //инжектим бин репозитория
 
     //Метод выборки из БД по условию идентификатора счета и пользователя
 }
