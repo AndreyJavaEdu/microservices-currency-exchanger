51c81c18d28ae3b0fdfd1d1db846069f49bdf0c4
AndreyJavaEd <scream123456@yandex.ru>
2023-12-19 17:27:51 +0300

    Создали класс AccountEventService который будет сервисом и будет реализовывать метод для получения списка операций из БД истории по операциям. Далее реализуем этот метод выборки операций в интерфейсе репозитория для дальнейшего его вызова в классе сервиса.

---
+++ history-service/src/main/kotlin/io/kamenskiyAndrey/history/service/AccountEventService.kt
@@ -0,0 +1,9 @@
+package io.kamenskiyAndrey.history.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class AccountEventService {
+
+    //Метод выборки из БД по условию идентификатора счета и пользователя
+}
