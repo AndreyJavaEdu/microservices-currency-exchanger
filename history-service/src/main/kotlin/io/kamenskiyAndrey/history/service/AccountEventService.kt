a705b2b44b8c191cfd671f7936f6621d0d06d2a1
AndreyJavaEd <scream123456@yandex.ru>
2023-12-19 19:52:50 +0300

    Сделали реализацию метода из репозитория по получению всех событий в классе сервиса AccountEventService - findAllByAccountEventsFromHistoryDB() .

--- history-service/src/main/kotlin/io/kamenskiyAndrey/history/service/AccountEventService.kt
+++ history-service/src/main/kotlin/io/kamenskiyAndrey/history/service/AccountEventService.kt
@@ -1,5 +1,6 @@
 package io.kamenskiyAndrey.history.service
 
+import io.kamenskiyAndrey.history.model.AccountEvent
 import io.kamenskiyAndrey.history.repository.AccountEventRepository
 import org.springframework.stereotype.Service
 
@@ -7,4 +8,9 @@ import org.springframework.stereotype.Service
 class AccountEventService(private val repository: AccountEventRepository) { //инжектим бин репозитория
 
     //Метод выборки из БД по условию идентификатора счета и пользователя
+    fun findAllByAccountEventsFromHistoryDB(accountId: Long): List<AccountEvent>{ //Идентификатор счета получаем из контроллера из вне
+       //TODO - нужно дописать получение  userId из сервиса аутентификации
+        val userId =
+       return repository.findAllByAccountIdAAndUserIdOrderByCreatedDesc(accountId, userId)
+    }
 }
