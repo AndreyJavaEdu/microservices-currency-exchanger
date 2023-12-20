b27ceeca81bbd6d7b01ab7d12b28a0101baad260
AndreyJavaEd <scream123456@yandex.ru>
2023-12-20 16:30:56 +0300

    Начали реализацию РЕСТ контроллера AccountHistoryController для отправки данных по http. Реализовал метод в контроллере, который вызывает метод сервиса по получению всех событий по операциям со счетом

--- history-service/src/main/kotlin/io/kamenskiyAndrey/history/controller/AccountHistoryController.kt
+++ history-service/src/main/kotlin/io/kamenskiyAndrey/history/controller/AccountHistoryController.kt
@@ -1,11 +1,18 @@
 package io.kamenskiyAndrey.history.controller
 
+import io.kamenskiyAndrey.history.model.AccountEvent
+import io.kamenskiyAndrey.history.service.AccountEventService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/history")
-class AccountHistoryController {
+class AccountHistoryController(private val historyService: AccountEventService) {
 
-
+    @GetMapping("/account/{id}")
+    fun findAllOperationsInAccountHistory(@PathVariable("id") accountId: Long): List<AccountEvent>{
+        return historyService.findAllByAccountEventsFromHistoryDB(accountId)
+    }
 }
