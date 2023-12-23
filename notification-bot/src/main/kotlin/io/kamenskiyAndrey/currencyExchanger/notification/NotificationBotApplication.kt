56d7ee7ecd4b43d5fe1c67c97db858204c68d852
AndreyJavaEd <scream123456@yandex.ru>
2023-12-23 14:51:40 +0300

    Init commit - new service of Notification bot.

---
+++ notification-bot/src/main/kotlin/io/kamenskiyAndrey/currencyExchanger/notification/NotificationBotApplication.kt
@@ -0,0 +1,11 @@
+package io.kamenskiyAndrey.currencyExchanger.notification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotificationBotApplication>(*args)
+}
