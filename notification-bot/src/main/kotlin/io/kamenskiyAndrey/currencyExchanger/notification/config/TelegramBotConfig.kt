264750439d43e9e7678236ecef174df2491f08c1
AndreyJavaEd <scream123456@yandex.ru>
2023-12-26 13:48:25 +0300

    Создали класс конфигурации TelegramBotConfig для получения настроек из файла конфигурации application.yml и контента из файла токена

---
+++ notification-bot/src/main/kotlin/io/kamenskiyAndrey/currencyExchanger/notification/config/TelegramBotConfig.kt
@@ -0,0 +1,32 @@
+package io.kamenskiyAndrey.currencyExchanger.notification.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+/*
+Класс конфигурации, чтобы прочитать конфигурацию из application.yml
+ */
+@Configuration
+class TelegramBotConfig {
+    private lateinit var appContext: ApplicationContext //инжектим данный бин, чтобы мы могли прочитать файлы из внешнего ресурса
+
+    //также подключим наши настройки которые мы указали в application.yml с помощью аннотации Value мы их достанем из файла application
+    @Value("\${telegram.botName}")
+    private val botName: String = ""
+
+    @Value("\${telegram.token}")
+    private val token: String = ""
+
+    //Функция чтения файла конфигурации и получение имени и токена
+    @Bean
+    fun botSettings(): BotSettings{
+    //реализуем чтение контента из файла с токеном
+        val resource = appContext.getResource("file:$token")
+        val content = if(resource.exists()) resource.file.readText()
+                    else throw RuntimeException("File $token not found !!!")
+        return BotSettings(botName, content) //заполняем бин именем бота и его токеном
+
+    }
+}
