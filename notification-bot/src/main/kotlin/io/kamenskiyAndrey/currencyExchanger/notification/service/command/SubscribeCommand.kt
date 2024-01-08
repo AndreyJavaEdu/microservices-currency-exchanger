ce7918c3b3f7a2f763474337c4d15436a6b9f98d
AndreyJavaEd <scream123456@yandex.ru>
2024-01-08 03:00:51 +0300

    Частично реализован метод по ответу на команду, которую вводим в телеграм бот для подписки (/subscribe) - processMessage(). В самом методе происходит получение сообщения который вводит пользователь, а именно его логин и пароль - эти данные парсятся из сообщения и передаются переменным. Далее эти данны в переменных присваюваются полям ДТО объекта в классе GetTokenCredititialsDTO для дальнейшего их использования в специальном сервисе по получению токена через RestTemplate. Метод не окончен - необходимо реализовать получение userId из payload токена.

--- notification-bot/src/main/kotlin/io/kamenskiyAndrey/currencyExchanger/notification/service/command/SubscribeCommand.kt
+++ notification-bot/src/main/kotlin/io/kamenskiyAndrey/currencyExchanger/notification/service/command/SubscribeCommand.kt
@@ -1,20 +1,37 @@
 package io.kamenskiyAndrey.currencyExchanger.notification.service.command
 
+import io.kamenskiyAndrey.currencyExchanger.notification.model.GetTokenCredititialsDTO
+import io.kamenskiyAndrey.currencyExchanger.notification.service.ServiceToGetUserId
 import io.kamenskiyAndrey.currencyExchanger.notification.service.SubscriptionService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service("subscribe")
 class SubscribeCommand(
+    val authService: ServiceToGetUserId,
 val subscriptionService: SubscriptionService
 ): BotCommandProcessor {
+    val logger: Logger = LoggerFactory.getLogger(SubscribeCommand::class.java)
     override fun processMessage(message: String, chatId: Long): String {
-        val (user, password) = message.split(":").let { array -> array[0] to array[1] }
+        val (user, password) = message.split(":")
+            .let { array -> array[0] to array[1] } // из введеного сообщения мы получаем логин и пароль пользователя
 
-        TODO("userId - необходимо реализовать получение")
-        if (userId != null) {
-            subscriptionService.subscribeUser(userId, chatId)
-            return "Подписка оформлена"
-        }else return "Подписка не оформлена, т.к. данный пользователь $user не существует!!!"
 
+
+        val credentials = GetTokenCredititialsDTO(name = user, password = password)
+
+
+        logger.info("Получение объекта ДТО - {}", credentials)
+
+        val token = authService.getToken(credentials)
+        logger.info("Получыенный токен - {}", token)
+        return "Для пробы"
+
+//        TODO("userId - необходимо реализовать получение")
+//        if (userId != null) {
+//            subscriptionService.subscribeUser(userId, chatId)
+//            return "Подписка оформлена"
+//        }else return "Подписка не оформлена, т.к. данный пользователь $user не существует!!!"
     }
 }
