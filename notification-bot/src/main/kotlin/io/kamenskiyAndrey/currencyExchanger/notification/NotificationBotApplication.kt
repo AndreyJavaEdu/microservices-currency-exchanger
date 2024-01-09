0e4bf9f4537da78bca6c30ac3064ce4e7c0a0dca
AndreyJavaEd <scream123456@yandex.ru>
2024-01-09 14:56:38 +0300

    Окончена реализация класса подписки пользователя в телеграмм боте SubscribeCommand. В этом классе инжектится бин ServiceToGetToken для получения токена, а также используется бин утилитарного самописного класса UtilJwtGettingPayload для вытаскиванияиз payload JWT token и получения userId.

--- notification-bot/src/main/kotlin/io/kamenskiyAndrey/currencyExchanger/notification/NotificationBotApplication.kt
+++ notification-bot/src/main/kotlin/io/kamenskiyAndrey/currencyExchanger/notification/NotificationBotApplication.kt
@@ -2,8 +2,10 @@ package io.kamenskiyAndrey.currencyExchanger.notification
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+//@EnableDiscoveryClient
 class NotificationBotApplication
 
 fun main(args: Array<String>) {
--- notification-bot/src/main/kotlin/io/kamenskiyAndrey/currencyExchanger/notification/service/command/SubscribeCommand.kt
+++ notification-bot/src/main/kotlin/io/kamenskiyAndrey/currencyExchanger/notification/service/command/SubscribeCommand.kt
@@ -1,37 +1,36 @@
 package io.kamenskiyAndrey.currencyExchanger.notification.service.command
 
 import io.kamenskiyAndrey.currencyExchanger.notification.model.GetTokenCredentialsDTO
-import io.kamenskiyAndrey.currencyExchanger.notification.service.ServiceToGetUserId
+import io.kamenskiyAndrey.currencyExchanger.notification.service.ServiceToGetToken
 import io.kamenskiyAndrey.currencyExchanger.notification.service.SubscriptionService
+import io.kamenskiyAndrey.currencyExchanger.notification.util.UtilJwtGettingPayload
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service("subscribe")
 class SubscribeCommand(
-    val authService: ServiceToGetUserId,
-val subscriptionService: SubscriptionService
-): BotCommandProcessor {
+    val utilJwt: UtilJwtGettingPayload,
+    val authService: ServiceToGetToken,
+    val subscriptionService: SubscriptionService
+) : BotCommandProcessor {
     val logger: Logger = LoggerFactory.getLogger(SubscribeCommand::class.java)
     override fun processMessage(message: String, chatId: Long): String {
         val (user, password) = message.split(":")
             .let { array -> array[0] to array[1] } // из введеного сообщения мы получаем логин и пароль пользователя
 
-
-
         val credentials = GetTokenCredentialsDTO(name = user, password = password)
-
-
         logger.info("Получение объекта ДТО - {}", credentials)
 
         val token = authService.getToken(credentials)
         logger.info("Получыенный токен - {}", token)
-        return "Для пробы"
-
-//        TODO("userId - необходимо реализовать получение")
-//        if (userId != null) {
-//            subscriptionService.subscribeUser(userId, chatId)
-//            return "Подписка оформлена"
-//        }else return "Подписка не оформлена, т.к. данный пользователь $user не существует!!!"
+        if (token != null) {
+            val userId = utilJwt.extractUserId(token)
+            logger.info("Полученный userId - {}", userId)
+            subscriptionService.subscribeUser(userId.toLong(), chatId)
+            return "Подписка оформлена"
+        } else {
+            throw Exception("Токен не обнаружен, возможно не аутентифицирован пользователь")
+        }
     }
 }
